46fc4a722fb07cb7f17eaf92aea1d08ddd0047b8
moyawuiki <106695988+moyawuiki@users.noreply.github.com>
2023-10-05 09:08:34 -0300

    05-10

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 alert('casa amarela');
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom(){ document.querySwlecto('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+function tocaSomTim(){ document.querySwlecto('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+function tocaSomPuff(){ document.querySwlecto('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+function tocaSomSplash(){ document.querySwlecto('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+function tocaSomToim(){ document.querySwlecto('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+function tocaSomPsh(){ document.querySwlecto('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+function tocaSomTic(){ document.querySwlecto('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+function tocaSomTom(){ document.querySwlecto('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+function tocaSomClap(){ document.querySwlecto('#som_tecla_clap').play();
+}
